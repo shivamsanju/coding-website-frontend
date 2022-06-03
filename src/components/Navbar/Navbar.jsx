16a700e8b6d7406ecf20184884ee99ef8c283242
Shivam Sanju <shvmsanju@gmail.com>
2022-06-04 00:54:33 +0530

    fixed urls

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ const iconPath = `${process.env.PUBLIC_URL}/assets/icons/`;
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <span className='header'><a className='github' href='https://github.com/shivamsanju'><img src={`${iconPath}logo.png`} alt="link" /></a>Leetcode Problems</span>
+        <span className='header'><img src={`${iconPath}logo.png`} alt="link" />Leetcode Problems</span>
         <div className="social">
-        <a className='github' href='https://github.com/shivamsanju'><img src={`${iconPath}linkedinicon.png`} alt="link" /></a>
+        <a className='github' href='https://www.linkedin.com/in/shvmsnju'><img src={`${iconPath}linkedinicon.png`} alt="link" /></a>
         <a className='github' href='https://github.com/shivamsanju'><img src={`${iconPath}githubicon.png`} alt="link" /></a>
         </div>
     </div>
